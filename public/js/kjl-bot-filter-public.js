af94275cfaa24ae72cb48f3ec06842809caf96b8
Gabriel Serwas <post@gabrielserwas.com>
2022-09-10 11:01:11 +0200

    Add javascript for sorting and filter.

--- public/js/kjl-bot-filter-public.js
+++ public/js/kjl-bot-filter-public.js
@@ -28,5 +28,75 @@
 	 * Although scripts in the WordPress core, Plugins and Themes may be
 	 * practising this, we should strive to set a better example in our own work.
 	 */
+	 var outputtext = document.getElementById('status');
+ 
+	 $(document).ready(function() {
+
+		function clearAllSortingInputs()
+		{
+			$('.books-filter').find('input:hidden').val('');
+		}
+		// $('#toggleswitch_djlp').val($(this).is(':checked'));
+		$('#toggleswitch_kimi').val($(this).is(':checked'));
+
+		$('#filter_author').on('click', function() {
+			clearAllSortingInputs();
+			var hiddenField = $('#author_input'),
+				val = hiddenField.val();
+		
+			hiddenField.val(val === "on" ? "off" : "on");
+		});
+
+		$('#filter_publisher').on('click', function() {
+			clearAllSortingInputs();
+			var hiddenField = $('#publisher_input'),
+				val = hiddenField.val();
+		
+			hiddenField.val(val === "on" ? "off" : "on");
+		});
+
+		$('#filter_title').on('click', function() {
+			clearAllSortingInputs();
+			var hiddenField = $('#title_input'),
+				val = hiddenField.val();
+		
+			hiddenField.val(val === "on" ? "off" : "on");
+		});
+
+		$('#filter_location').on('click', function() {
+			clearAllSortingInputs();
+			var hiddenField = $('#location_input'),
+				val = hiddenField.val();
+		
+			hiddenField.val(val === "on" ? "off" : "on");
+		});
+
+
+		$('#toggleswitch_djlp').on('click',function(){
+			var hiddenField = $('#toggleswitch_djlp_input'),
+			val = hiddenField.val();
+	
+			hiddenField.val(val === "on" ? "off" : "on");
+			if(this.checked) {
+				$(this).prop('value', 'on');
+			} else {
+				$(this).prop('value', 'off');
+			}
+			$(this).parents('form:first').submit();
+		});
+
+		$('#toggleswitch_kimi').on('click',function(){
+			var hiddenField = $('#toggleswitch_kimi_input'),
+			val = hiddenField.val();
+	
+			hiddenField.val(val === "on" ? "off" : "on");
+			if(this.checked) {
+				$(this).prop('value', 'on');
+			} else {
+				$(this).prop('value', 'off');
+			}
+			$(this).parents('form:first').submit();
+		});
+	});
 
 })( jQuery );
